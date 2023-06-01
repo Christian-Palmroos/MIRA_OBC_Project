ad981a0f7f2d7dd5f0b09b51c4632f11f5e45f9f
sevenyberg <shonyb@utu.fi>
2023-06-01 14:46:20 +0300

    Added blinking LEDs.

--- miracode/Core/Src/main.c
+++ miracode/Core/Src/main.c
@@ -119,7 +119,14 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  HAL_GPIO_TogglePin (LED0_GPIO_Port, LED0_Pin);
+	  HAL_Delay (300);
+	  HAL_GPIO_TogglePin (LED1_GPIO_Port, LED1_Pin);
+	  HAL_Delay (300);
+	  HAL_GPIO_TogglePin (LED2_GPIO_Port, LED2_Pin);
+	  HAL_Delay (300);
+	  HAL_GPIO_TogglePin (LED3_GPIO_Port, LED3_Pin);
+	  HAL_Delay (300);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
