b58331de757cb2bac6c537862ecf18b8b55e50c4
sevenyberg <shonyb@utu.fi>
2023-09-12 14:37:51 +0300

    Removed redundancies, added MIRA libraries and code blocks.

---
+++ miracode/Core/Inc/mira.h
@@ -0,0 +1,14 @@
+/*
+ * mira.h
+ *
+ *  Created on: Sep 12, 2023
+ *      Author: shonyb
+ */
+
+#ifndef INC_MIRA_H_
+#define INC_MIRA_H_
+
+void mira_read();
+void mira_init();
+
+#endif /* INC_MIRA_H_ */
