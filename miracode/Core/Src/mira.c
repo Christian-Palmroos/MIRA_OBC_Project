f25f2b784d8f2e4a5323d3eb537f4d04f211751e
sevenyberg <shonyb@utu.fi>
2023-09-13 15:08:48 +0300

    Added some more content to mira.c, not ready yet.

--- miracode/Core/Src/mira.c
+++ miracode/Core/Src/mira.c
@@ -5,35 +5,64 @@
  *      Author: shonyb
  */
 
-void mira_read(){
+void mira_read(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout){
+
 	// read register at given address and set to given pointer
+	HAL_UART_Receive(huart, pData, Size, Timeout);
+
 	// return status
 
 }
 
-void mira_write(){
+void mira_write(UART_HandleTypeDef *huart, const uint8_t *pData, uint16_t Size, uint32_t Timeout){
+
 	// write given value to register at given address
+	HAL_UART_Transmit(huart, pData, Size, Timeout);
+
 	// return status
+
 }
 
-void mira_science_data(){
+void mira_science_data(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout){
+
 	// mira_read() to given pointer
+	mira_read(huart, pData, Size, Timeout);
+
 	// set mark_as_read
+	mira_write(huart, pData, Size, Timeout);
+
 	// return status
 
 }
 
-void mira_housekeeping_data(){
+void mira_housekeeping_data(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout){
+
 	// mira_read() to given pointer
+	mira_read(huart, pData, Size, Timeout);
+
 	// return status
 
 }
 
 void mira_init(){
+
+	// Turn on MIRA power (GPIO)
+	HAL_GPIO_TogglePin (MIRA_EN_PWR_GPIO_Port, MIRA_EN_PWR_Pin);
+
 	// OBC queries MIRA time
+	mira_read(huart, pData, Size, Timeout);
+
 	// activate powersave
+	mira_write(huart, pData, Size, Timeout);
+
 	// OBC writes configuration data to MIRA registers
-	// OBS sets MIRA time
+	mira_write(huart, pData, Size, Timeout);
+
+	// OBC sets MIRA time
+	mira_write(huart, pData, Size, Timeout);
+
 	// return from powersave
+	mira_write(huart, pData, Size, Timeout);
+
 }
 
