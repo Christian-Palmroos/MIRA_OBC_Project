3b131a0d77af69426d8309b3dceec86fcfb7beda
Christian-Palmroos <chospa@utu.fi>
2023-10-25 12:57:13 +0300

    Added MIRA commands and responses

--- miracode/Core/Src/mira.c
+++ miracode/Core/Src/mira.c
@@ -5,6 +5,24 @@
  *      Author: shonyb
  */
 
+// Command codes to MIRA
+#define STATUS_REQUEST 0x00
+#define READ_REGISTER 0x02
+#define WRITE_REGISTER 0x03
+#define GET_HOUSEKEEPING 0x30
+#define	GET_SCIENCE_DATA 0x40
+#define	GET_CALBRATION_DATA 0x42
+#define	SET_TIME 0x50
+#define GET_TIME 0x51
+
+// MIRA responses
+#define STATUS_PACKET 0x71
+#define REGISTER_CONTENT 0x72
+#define HK_PACKET 0x31
+#define SCIENCE_DATA_PACKET 0x41
+#define TIME_PACKET 0x52
+#define	CALIBRATION_PACKET 0x82
+
 void mira_read(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout){
 
 	// read register at given address and set to given pointer
